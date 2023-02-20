b44c9666c0d7e768d1bc0ad6c48a42853f78ace2
Ana Schlienger <anaiss@theodo.com>
2023-02-20 13:13:52 -0500

    clean solution backlog 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import logo from './logo.svg';
 import loading from './loading.svg';
 import './App.css';
 import Sound from 'react-sound';
-import Button from './Button';
 
 const apiToken = 'BQBXlpR83yFORFKYF-wKNZXLLvcOrympGeeg31UxVsANpNewhePkObrGbjE8nX85IRfk3AbjrO4Hc4DJp4ISF0E_LEvgKZcd3nnrT2jB6DPpiV3Kh1wVTDPmhomC7GWNPwlkTPr-voHROVdeAMKovnDxG4Zc936yM1mQ7Y0Zh17eILBirpq4-OFxTP23GnqkBYPTP7dDY2VnkQ'
 
@@ -30,7 +29,6 @@ function getRandomNumber(x) {
 
 const AlbumCover = (props) =>  {
   const src = props.currentTrack.album.images[0].url; 
-  console.log('src album', src)
     return (
         <img src={src} style={{ width: 400, height: 400 }} />
     );
@@ -41,7 +39,6 @@ const App = () => {
   const [currentTrack, setCurrentTrack] = useState([]);
   const [tracksChoice, setTracksChoice] = useState([]);
   const [songsLoaded, setSongsLoaded] = useState(false);
-  const [shouldPlay, setShouldPlay] = useState(false);
 
   useEffect(() => { 
     fetch('https://api.spotify.com/v1/me/tracks', {
@@ -52,12 +49,10 @@ const App = () => {
     })
     .then(response => response.json())
     .then((data) => {
-      console.log("Reply received! This is what I received: ", data);
       setTracks(shuffleArray(data.items))
       setCurrentTrack(data.items[0].track)
       setTracksChoice(shuffleArray([data.items[0].track, data.items[1].track, data.items[2].track]))
       setSongsLoaded(true)
-      console.log('trakcs')
     })
   }, []);
   
@@ -86,18 +81,13 @@ const App = () => {
       </header>
       <div className="App-images">
         <AlbumCover currentTrack={currentTrack} />
-        {shouldPlay && <Sound url={tcurrentTrack.preview_url} playStatus={Sound.status.PLAYING} />}
+        <Sound url={currentTrack.preview_url} playStatus={Sound.status.PLAYING} />
       </div>
-      <div className="App-buttons">
-        <button onClick={() => setShouldPlay((previousValue) => !previousValue)}>
-          Play
-        </button>
         <div className="App-buttons">
-          {tracksChoice.map((track) => (
-            <button onClick={() => checkAnswer(currentTrack, track)}> {track.name} </button>
-          ))}
+        {tracksChoice.map((track) => (
+          <button onClick={() => checkAnswer(currentTrack, track)}> {track.name} </button>
+        ))}
         </div>
-      </div>
     </div>
   );
 };
